b774caf9afcee03b37a8da7548f0fdd6b64fed59
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-09-17 13:57:32 -0400

    set default for airport

--- models/flight.js
+++ models/flight.js
@@ -3,19 +3,28 @@ import mongoose from "mongoose";
 const flightSchema = new mongoose.Schema({
   airline: {
     type: String,
-    enum: ['American', 'Southwest', 'United']
+    enum: ['American', 'Southwest', 'United'],
+    required: true
   },
   airport: {
     type: String,
-    enum: ['AUS','DFW','DEN','LAX','SAN']
+    enum: ['AUS','DFW','DEN','LAX','SAN'],
+    required: true,
+    default: function() {
+      return 'DEN'
+    }
   },
   flightNo: {
     type: Number, 
     min: 10,
     max: 9999,
   },
-  departs: Date,
+  departs: {
+    type: Date,
+    required: true
+  },
 })
+
 
 const Flight = mongoose.model('Flight', flightSchema)
 
